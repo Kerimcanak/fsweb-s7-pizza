ea79fec9792873b5cdbf3f22f898380b103910cd
Kerim <kerimcanak@gmail.com>
2024-06-18 23:20:37 +0300

    trying to get UI better

--- src/assets/pizzaForm/headerForm.jsx
+++ src/assets/pizzaForm/headerForm.jsx
@@ -7,21 +7,33 @@ import logo from './logo.svg';
  * @return {JSX.Element} The header component.
  */
 const HeaderForm = () => (
-  <header style={{
-    paddingTop: '50px',
-    paddingBottom: '25px',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '0em',
-    margin: 0,
-    backgroundColor: '#c20608'
-  }}>
-    <img src={logo} alt="logo" style={{ width: '350px', margin: '0 auto'}}/>
+    <header style={{
+      paddingTop: '50px',
+      paddingBottom: '25px',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '0em',
+      margin: 0,
+      backgroundColor: '#c20608'
+    }}>
+    <div style={{ height: '50px', backgroundColor: '#c20608' }} />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" style={{ width: '350px', margin: '0 auto' }} />
+    </div>
+    <div style={{ height: '20px', backgroundColor: '#c20608' }} />
+    <div style={{ textAlign: 'left' }}>
+      <h1 style={{ fontSize: '16px', margin: 0, color: 'white', paddingLeft: '1em', alignSelf: 'flex-start' }}>
+        Anasayfa - Seçenekler - Sipariş Oluştur
+      </h1>
+    </div>
   </header>
 );
 
 export default HeaderForm;
+
