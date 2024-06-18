84eb82ff9d81de55ff84a89ba428b5cbf7859290
Kerim <kerimcanak@gmail.com>
2024-06-19 00:02:02 +0300

    initilised the framework of pizza-order page

--- src/assets/pizzaForm/headerForm.jsx
+++ src/assets/pizzaForm/headerForm.jsx
@@ -7,33 +7,40 @@ import logo from './logo.svg';
  * @return {JSX.Element} The header component.
  */
 const HeaderForm = () => (
-    <header style={{
-      paddingTop: '50px',
-      paddingBottom: '25px',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: '0em',
-      margin: 0,
-      backgroundColor: '#c20608'
-    }}>
+  <header style={{
+    paddingTop: '50px',
+    paddingBottom: '25px',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '0em',
+    margin: 0,
+    backgroundColor: '#c20608'
+  }}>
     <div style={{ height: '50px', backgroundColor: '#c20608' }} />
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={logo} alt="logo" style={{ width: '350px', margin: '0 auto' }} />
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <img src={logo} alt="logo" style={{ width: '350px', margin: '0 auto', alignSelf: 'center' }} />
+    </div>
+    <div style={{ height: '40px', backgroundColor: '#c20608' }} />
+    <div style={{
+      display: 'flex',
+      alignItems: 'left',
+      justifyContent: 'left',
+      textAlign: 'left',
+      width: '100%',
+      paddingLeft: '60em',
+      color: 'white', // Added color property to make text white
+      backgroundColor: '#c20608', // Added background color to match header background
+    }}>
+      Anasayfa - Seçenekler - Sipariş Oluştur
     </div>
     <div style={{ height: '20px', backgroundColor: '#c20608' }} />
-    <div style={{ textAlign: 'left' }}>
-      <h1 style={{ fontSize: '16px', margin: 0, color: 'white', paddingLeft: '1em', alignSelf: 'flex-start' }}>
-        Anasayfa - Seçenekler - Sipariş Oluştur
-      </h1>
-    </div>
   </header>
 );
 
 export default HeaderForm;
-
