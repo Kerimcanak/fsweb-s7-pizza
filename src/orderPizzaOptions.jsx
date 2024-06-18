79750d82ce2eac39bd5178941ea9d788d63c575a
Kerim <kerimcanak@gmail.com>
2024-06-19 02:47:37 +0300

    initialised reactstrap

--- src/orderPizzaOptions.jsx
+++ src/orderPizzaOptions.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import HeaderForm from './assets/pizzaForm/headerForm';
 import HeaderTextMain from './assets/pizzaForm/headerText';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const OrderPizza = () => {
   return (
     <div>
       <HeaderForm style={{ marginBottom: '20px' }} />
       <HeaderTextMain />
-      <FormGroup>
-          <Label for="userName">İsim</Label>
-          <Input type="name" name="isim" id="userName" placeholder="name placeholder" />
-        </FormGroup>
     </div>
   );
 }
