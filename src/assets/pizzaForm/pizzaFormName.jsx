0fa3a27348f97503281b8a5f30a3c74f9f1e324e
Kerim <kerimcanak@gmail.com>
2024-06-19 04:17:32 +0300

    added notes

--- src/assets/pizzaForm/pizzaFormName.jsx
+++ src/assets/pizzaForm/pizzaFormName.jsx
@@ -8,13 +8,15 @@ const PizzaFormName = () => {
     const inputName = event.target.value;
     setName(inputName);
     setValidName(inputName.length >= 3);
+    //handlechange observes valid name
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
-
+  //there is a weird syntax in here
   return (
+    //not sure if you need this
     <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
       <form onSubmit={handleSubmit}>
         <label style={{ 
@@ -36,6 +38,7 @@ const PizzaFormName = () => {
             padding: "10px", // add some padding for better usability
             width: "350px" // adjust the width to your liking 
             }}
+            //you should add a validation
         />
         {!validName && name.length > 0 && (
           <div style={{ color: "red", fontFamily: "Roboto Condensed" }}>
