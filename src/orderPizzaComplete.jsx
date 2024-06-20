a624ea26445b6a04c8853bb10714b576058736b7
Kerim <kerimcanak@gmail.com>
2024-06-20 06:29:23 +0300

    created subpages for options section

--- src/orderPizzaComplete.jsx
+++ src/orderPizzaComplete.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HeaderForm from './assets/pizzaForm/headerForm';
-import HeaderTextMain from './assets/pizzaForm/headerText';
+import HeaderTextOrder from './assets/pizzaForm/headerTextOrder';
 
 const OrderPizzaComplete = () => {
   return (
     <div>
       <HeaderForm style={{ marginBottom: '20px' }} />
-      <HeaderTextMain />
+      <HeaderTextOrder />
     </div>
   );
 }
